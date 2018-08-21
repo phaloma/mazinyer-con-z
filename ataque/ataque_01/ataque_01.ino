c4c09ec94f7f6f1a0911e40fe2de35c54c8eff4b
phaloma <38531425+phaloma@users.noreply.github.com>
2018-08-21 18:27:38 -0300

    copilado de cosas uwu
    codigo de rueditas + ataque +apk+flechas

---
+++ ataque/ataque_01/ataque_01.ino
@@ -0,0 +1,48 @@
+#include <Servo.h>
+char nombre[10] = "Mazinger"; //El nuevo nombre del modulo
+char password [10] = "1234"; //El nuevo password. No es muy seguro...
+char baud = '4'; //4 = 9600 baud
+
+int ang=0;//posicion inicial 
+int lA = 0;
+Servo servomotor;
+
+void setup() {
+  
+  Serial.begin (9600);//inicial e serial 
+  servomotor.attach(9,600,2400);//servo en el pin9 &AP(min)=600 AP(max)=2400
+}
+
+void loop(){
+  
+  if(Serial.available() >= 1){ //detecta una entrada
+    char entrada = Serial.read(); //lee el caracter del celular 
+    Serial.println(entrada);
+
+    lA = 0;
+    
+    if (entrada == 'U'){
+      lA=1;
+      }
+   
+   
+  }//Leer el bt
+  
+  //acciones finales
+  //Condicionales
+  if(lA== 1){
+    ang += 5;
+    }
+
+  else{
+    ang-=5;
+    }
+    
+  ang=constrain(ang,0,180);//limitacion de los valores del angulo
+  
+  Serial.println(ang);
+      
+   servomotor.write(ang);//se ingresa el valor "ang" al servo
+   delay(5);//por si las moscas   
+  }
+
